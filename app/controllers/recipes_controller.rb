72e80d3f632c6ac3f5dd926a620ca7a1e49b2a89
dani <dani.zaghian@gmail.com>
2022-01-13 13:07:35 -0800

    adds index and show routes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,17 @@
 class RecipesController < ApplicationController
 
-  def first_recipe_method
-    recipe = Recipe.first # an instance/hash
+  # def first_recipe_method
+  #   recipe = Recipe.first # an instance/hash
+  #   render json: recipe.as_json
+  # end
+
+  def index
+    recipes = Recipe.all
+    render json: recipes.as_json
+  end
+
+  def show
+    recipe = Recipe.find(params[:id])
     render json: recipe.as_json
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get "/first_recipe_path", controller: "recipes", action: "first_recipe_method"
+  # get "/first_recipe_path", controller: "recipes", action: "first_recipe_method"
+
+  get "/recipes" => "recipes#index"
+  get "/recipes/:id" => "recipes#show"
 end
+
