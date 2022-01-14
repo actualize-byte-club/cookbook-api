1392fc60b050c1293f36046f412406103b322abf
dani <dani.zaghian@gmail.com>
2022-01-14 11:54:21 -0800

    adds update and destroy actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,4 +22,27 @@ class RecipesController < ApplicationController
     render json: recipe.as_json
   end
 
+  def update
+    # Find the right recipe using the url segment parameter
+    recipe = Recipe.find(params[:id])
+    # Update the recipe with the body parameters
+    recipe.title = params[:title] || recipe.title
+    recipe.chef = params[:chef] || recipe.chef
+    recipe.ingredients = params[:ingredients] || recipe.ingredients
+    recipe.directions = params[:directions] || recipe.directions
+    recipe.prep_time = params[:prep_time] || recipe.prep_time
+    recipe.image_url = params[:image_url] || recipe.image_url
+    # Save the recipe changes to the database
+    recipe.save
+    render json: recipe.as_json
+  end
+
+  def destroy
+    # Find the right recipe using the url segment parameter
+    recipe = Recipe.find(params[:id])
+    # Delete the recipe
+    recipe.destroy
+    render json: {message: "Recipe successfully destroyed!"}
+  end
+
 end
