3684b4caf54caad98a45af9f277d8c3dccbde301
dani <dani.zaghian@gmail.com>
2022-01-24 11:09:12 -0800

    adds user signup with bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get "/recipes/:id" => "recipes#show"
   patch "/recipes/:id" => "recipes#update"
   delete "/recipes/:id" => "recipes#destroy"
+
+  post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 end
 
