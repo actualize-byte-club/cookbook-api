940784d0e3888892883e39b648d4b20e7e11c5c1
dani <dani.zaghian@gmail.com>
2022-02-16 10:37:50 -0800

    updates recipe serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,5 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :title, :ingredients, :directions, :prep_time, :image_url, :ingredients_list, :directions_list, :friendly_created_at, :friendly_prep_time, :user_id
+  attributes :id, :title, :ingredients, :directions, :prep_time, :image_url, :ingredients_list, :directions_list, :friendly_created_at, :friendly_prep_time, :user_id, :created_at
   
   attribute :owner, if: :current_user
 
